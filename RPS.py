7839ca2be0718b61589ea14ddf8ec303bdd71243
Adeleke Oladapo Joseph <oladapo_joseph@yahoo.com>
2020-11-14 21:31:41 +0100

    p1:60%+, p2 and p3:~50, p4:70%

--- RPS.py
+++ RPS.py
@@ -2,21 +2,31 @@
  # whatever the opponent played two plays ago. It is not a very good player
  # so you will need to change the code to pass the challenge.
 import random
+play = []
 def player(prev_play, opponent_history=[]):
     opponent_history.append(prev_play)
 
-    guess = "R"
-    # if len(opponent_history) == 2:
-    #     guess= 'P'
 
     if len(opponent_history) > 2:
-        if opponent_history[-1] == opponent_history[-2] == ('S' or 'P'):
-            guess = "R"
+        if opponent_history[-1] == opponent_history[-2] =='S':
+            guess = random.choice(["R", opponent_history[-3]])
+            # guess = 'R'
             return guess
-        if opponent_history[-1] == opponent_history[-2] == ('R'):
-            guess = random.choice(["R", "S"])
+        elif opponent_history[-1] == opponent_history[-2] == 'R':
+            guess = random.choice(["P",opponent_history[-3]])
+            # guess = 'P'
             return guess
-        guess =opponent_history[-(random.randint(1, len(opponent_history)))]
-        if
-        return guess
+        elif opponent_history[-1] == opponent_history[-2] == 'P':
+            guess = random.choice(["S", opponent_history[-3]])
+            # guess = 'S'
+            return guess
+        else:
+            # guess = opponent_history[-(random.randint(1,4))]
+            # guess = random.choice(opponent_history)
+            guess = random.choice(['R', 'P', 'S'])
+            return guess
+    else:
+        guess = 'P'
+
+    play.append(guess)
     return guess
